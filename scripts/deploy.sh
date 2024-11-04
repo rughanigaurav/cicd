f6747c3edef8fa7e3a953b1c7b39aa218fba0919
GauravProlix <gaurav.prolix@gmail.com>
2024-11-04 18:33:06 +0530

    added sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # Change to your project directory
-cd /home/ubuntu/project/linx-frontnend/cicd || exit
+cd /home/ubuntu/project/linx-frontend/ || exit
+
+# Remove existing appspec.yml if it exists
+if [ -f "cicd/appspec.yml" ]; then
+    echo "Removing existing appspec.yml..."
+    rm -f "cicd/appspec.yml"
+fi
 
 # Pull the latest code
 echo "Pulling latest code..."
-git pull origin main
+git pull origin main  # Change 'main' if your branch name is different
 
+# Install dependencies and run production build
 echo "Installing dependencies and running production build..."
 npm install
 npm run prod
