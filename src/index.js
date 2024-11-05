21e01c86649177e20b89c0c1f1ce8119430eca44
GauravProlix <gaurav.prolix@gmail.com>
2024-11-05 19:19:04 +0530

    testing branch changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, from cicd staging branch");
+  res.send("Hello, from cicd main branch");
 });
 
 app.listen(PORT, () => {
