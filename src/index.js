ff16c258db862b2f2adec33c9c1da05ce4aebd7d
GauravProlix <gaurav.prolix@gmail.com>
2024-11-05 19:06:37 +0530

    staging branch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, from cicd server");
+  res.send("Hello, from cicd staging branch");
 });
 
 app.listen(PORT, () => {
