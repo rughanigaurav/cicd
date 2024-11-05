40634ee2698d29ac7d338f8de7b6abccfd85c477
GauravProlix <gaurav.prolix@gmail.com>
2024-11-05 15:21:19 +0530

    changes in deploy.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,4 +5,4 @@ cd /home/ubuntu/project/linx-frontend/cicd/ || exit
 
 echo "stop the node application...."
 
-pm2 stop src/index.js
+sudo supervisorctl stop all
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # Change to your project directory
 cd /home/ubuntu/project/linx-frontend/cicd/ || exit
 
-sudo fuser -k 8000/tcp
+sudo supervisorctl restart all
 
 echo "Start Application...."
 pm2 start src/index.js
