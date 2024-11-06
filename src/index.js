c28f581f2b3c6811cf2b4746b81eb4388f3c13b1
GauravProlix <gaurav.prolix@gmail.com>
2024-11-06 11:58:46 +0530

    test commit 11

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ const PORT = 8000;
 
 app.get("/", (req, res) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
   res.send("Hello, from cicd test14 branch checkout staging--14");
 =======
   res.send("Hello, from cicd test4 branch checkout staging--4");
 >>>>>>> 07772e4 (added port kill command to free 8000 test commit 4)
+=======
+  res.send("Hello, from cicd test6 branch checkout staging--6");
+>>>>>>> 2f3b9a3 (changes in appspec test 6)
 });
 
 app.listen(PORT, () => {
