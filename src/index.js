2f3b9a3a38a0cc8793d2b942f44e9a8801d19bda
GauravProlix <gaurav.prolix@gmail.com>
2024-11-06 11:58:46 +0530

    changes in appspec test 6

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, from cicd test4 branch checkout staging--4");
+  res.send("Hello, from cicd test6 branch checkout staging--6");
 });
 
 app.listen(PORT, () => {
