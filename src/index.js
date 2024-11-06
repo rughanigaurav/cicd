8ecaeb19a9bb504f6c1d31d3820e0dc4f06a9e08
GauravProlix <gaurav.prolix@gmail.com>
2024-11-06 10:59:17 +0530

    changes in main

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ const app = express();
 const PORT = 8000;
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
   res.send("Hello, from cicd test7777 branch checkout staging");
-=======
-  res.send("Hello, from cicd test7777 branch checkout staging");
->>>>>>> staging
 });
 
 app.listen(PORT, () => {
