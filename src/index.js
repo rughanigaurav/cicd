b07cfd345e472205f1f8301c59a1cfec57f282dc
GauravProlix <gaurav.prolix@gmail.com>
2024-11-06 12:48:47 +0530

    test commit 11

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const PORT = 8000;
 app.get("/", (req, res) => {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   res.send("Hello, from cicd test14 branch checkout staging--14");
 =======
   res.send("Hello, from cicd test4 branch checkout staging--4");
@@ -12,6 +13,9 @@ app.get("/", (req, res) => {
 =======
   res.send("Hello, from cicd test6 branch checkout staging--6");
 >>>>>>> 2f3b9a3 (changes in appspec test 6)
+=======
+  res.send("Hello, from cicd test12 branch checkout staging--12");
+>>>>>>> 76d0350 (test commit 11)
 });
 
 app.listen(PORT, () => {
