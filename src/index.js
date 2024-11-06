5ac8ede8c0221a25921257d335238d0dab74bf08
GauravProlix <gaurav.prolix@gmail.com>
2024-11-06 11:31:25 +0530

    revert commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 8000;
 
 app.get("/", (req, res) => {
+<<<<<<< HEAD
   res.send("Hello, from cicd test14 branch checkout staging--14");
+=======
+  res.send("Hello, from cicd test4 branch checkout staging--4");
+>>>>>>> 07772e4 (added port kill command to free 8000 test commit 4)
 });
 
 app.listen(PORT, () => {
